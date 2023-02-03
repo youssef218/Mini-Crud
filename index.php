32c645973f6c57985c64254150f7a75615350ea2
Youssef-Bouayez <77173016+youssef218@users.noreply.github.com>
2023-02-03 11:08:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+    //inclure la page de connexion
+    include_once "con_bdd.php";
+    //verifier que les données sont envoyés
+    if(isset($_POST['send'])){
+        //verifiez que l'image et le texte ont été choisies
+        if(!empty($_FILES['image']) && isset($_POST['text']) && $_POST['text']!= ""){
+            
+            //On récupère d'abord le nom de l'image
+            $img_nom = $_FILES['image']['name'];
+
+            //Nous définissons un nom temporaire
+            $tmp_nom = $_FILES['image']['tmp_name'];
+
+            //On récupère l'heure actuelle
+            $time = time();
+
+            //On rennomme l'image en utilisant cette formule : heure + nom de l'image (Pour avoir des images uniques)
+            $nouveau_nom_img = $time.$img_nom ;
+
+            //on déplace l'image dans un dossier appellé "image_bdd"
+            $deplacer_img = move_uploaded_file($tmp_nom,"image_bdd/".$nouveau_nom_img);
+
+            if($deplacer_img){
+                //si l'image a été mis dans le dossier 
+                //insérons le texte et le nom de l'image dans la base de données
+                $text = $_POST['text'] ;
+                $req = mysqli_query($con , "INSERT INTO images VALUES (NULL ,'$nouveau_nom_img','$text')");
+                //verifier que la requête fonctionne
+                if($req){
+                    //si oui , faire une redirection vers la page liste.php
+                    header("location:liste.php") ;
+                }else {
+                    //si non
+                    $message = "Echec de l'ajout de l'image !";
+                }
+            }else {
+                //si non
+                $message = "Veuillez choisir une image avec une taille inférieur à 1Mo !";
+            }
+
+        }else {
+            //si les champs sont vides on affiche un message
+            $message = "Veuillez remplir tous les champs !";
+        }
+    }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini CRUD</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="liste.php" class="link">Liste des photos</a>
+    <p class="error">
+        <?php 
+           //afficher une erreur si la variable message existe
+           if(isset($message)) echo $message ;
+        ?>
+    </p>
+    <form action="" method="POST" enctype="multipart/form-data"> 
+        <label>Ajouter une photo</label>
+        <input type="file" name="image">
+        <label>Description</label>
+        <textarea name="text" cols="30" rows="10"></textarea>
+        <input type="submit" value="Ajouter" name="send">
+    </form>
+</body>
+</html>
---
+++ liste.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste photos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <a href="index.php" class="link">Ajouter une photo</a>
+        <?php
+            //inclure la page de connexion
+            include_once "con_bdd.php";
+            //afficher la liste des photos qui sont dans la base de donnée
+            $req = mysqli_query($con , "SELECT * FROM images");
+
+            //verifier que la liste n'est pas vide
+            if(mysqli_num_rows($req) < 1){
+                ?>
+                <p class="vide_message">La liste des photos est vide.</p>
+                <?php
+            }
+
+            //afficher la liste des photos
+            while($row = mysqli_fetch_assoc($req)){
+                ?>         
+                    <div class="box">
+                        <img  class="img_principal" src="image_bdd/<?=$row['img']?>">
+                        <div><?=$row['txt']?></div>
+                        <a class="delete_btn" href="delete.php?id=<?=$row['id']?>">
+                            <img src="remove.png">
+                        </a>
+                    </div>
+                <?php
+            }
+        ?>
+
+    </section>
+</body>
+</html>
